90c0459a366eed2bb82b0a25e2541d1c07745f29
chenyb46701 <chenyb46701@hundsun.com>
2022-09-14 19:47:58 +0800

    [提交类型]:代码整理 [修改版本]: [需求单号]: [缺陷编号]: [修改内容说明]: 整理后端代码框架

---
+++ src/main/java/com/hdu/lease/core/InitContract.java
@@ -0,0 +1,28 @@
+package com.hdu.lease.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 初始化合约
+ *
+ * @author chenyb46701
+ * @date Ap2/9/13
+ */
+@Slf4j
+@Component
+public class InitContract implements ApplicationRunner {
+
+    /**
+     * 部署合约
+     *
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
