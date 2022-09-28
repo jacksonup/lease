fafb163cfeedd0d39bbd4faf86048c6919b0a74f
jiazhenyu123 <57209889+jiazhenyu123@users.noreply.github.com>
2022-09-28 13:52:11 +0800

    1.提交UserContract类

--- src/main/java/com/hdu/lease/contract/UserContract.java
+++ src/main/java/com/hdu/lease/contract/UserContract.java
@@ -299,6 +299,44 @@ public class UserContract extends Contract {
         return deployRemoteCall(UserContract.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
     }
 
+    public static class User extends DynamicStruct {
+        public String account;
+
+        public String name;
+
+        public String phone;
+
+        public String password;
+
+        public BigInteger isBindPhone;
+
+        public BigInteger role;
+
+        public BigInteger status;
+
+        public User(String account, String name, String phone, String password, BigInteger isBindPhone, BigInteger role, BigInteger status) {
+            super(new org.web3j.abi.datatypes.Utf8String(account),new org.web3j.abi.datatypes.Utf8String(name),new org.web3j.abi.datatypes.Utf8String(phone),new org.web3j.abi.datatypes.Utf8String(password),new org.web3j.abi.datatypes.generated.Uint256(isBindPhone),new org.web3j.abi.datatypes.generated.Uint256(role),new org.web3j.abi.datatypes.generated.Uint256(status));
+            this.account = account;
+            this.name = name;
+            this.phone = phone;
+            this.password = password;
+            this.isBindPhone = isBindPhone;
+            this.role = role;
+            this.status = status;
+        }
+
+        public User(Utf8String account, Utf8String name, Utf8String phone, Utf8String password, Uint256 isBindPhone, Uint256 role, Uint256 status) {
+            super(account,name,phone,password,isBindPhone,role,status);
+            this.account = account.getValue();
+            this.name = name.getValue();
+            this.phone = phone.getValue();
+            this.password = password.getValue();
+            this.isBindPhone = isBindPhone.getValue();
+            this.role = role.getValue();
+            this.status = status.getValue();
+        }
+    }
+
     public static class LogEventResponse extends BaseEventResponse {
         public String message;
 
@@ -311,3 +349,4 @@ public class UserContract extends Contract {
         public User user;
     }
 }
+
